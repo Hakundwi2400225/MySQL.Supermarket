2ef50a74f288193430077ad1300fe09080d00126
Hakundwi2400225 <givenhakundwi7@gmail.com>
2026-04-22 13:30:56 +0200

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,22 +1,23 @@
 
+SET time_zone = '+02:00';
 
 -- GROUP-19 SUPERMARKET --
 
 -- CUSTOMER STORAGE TABLE
 CREATE TABLE Customer (
-    customer_id INT PRIMARY KEY AUTO_INCREMENT,
+    customerID INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(100) NOT NULL,
-    Phone VARCHAR(20),
-    Email VARCHAR(100),
+    Phone VARCHAR(20) unique,
+    Email VARCHAR(100) unique,
     Address VARCHAR(100)
 );
 
--- CUSTOMER TRACKER FOR RECORD CHANGESS ON THE TABLE LIST
+-- CUSTOMER TRACKER FOR RECORD CHANGES ON THE TABLE LIST
 CREATE TABLE Customer_Audit (
-    audit_id INT PRIMARY KEY AUTO_INCREMENT,
-    customer_id INT,
+    auditID INT PRIMARY KEY AUTO_INCREMENT,
+    customerID INT,
     action VARCHAR(10),
-    action_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    action_time TIMESTAMP,
     name VARCHAR(100),
     Phone VARCHAR(20),
     Email VARCHAR(100),
@@ -25,22 +26,22 @@ CREATE TABLE Customer_Audit (
 
 -- EMPLOYEE STORAGE TABLE
 CREATE TABLE Employee (
-    employee_id INT PRIMARY KEY AUTO_INCREMENT,
+    employeeID INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(100) NOT NULL,
     workTitle VARCHAR(50),
-    Phone VARCHAR(20),
-    Email VARCHAR(100),
+    Phone VARCHAR(20) unique,
+    Email VARCHAR(100) unique,
     Address VARCHAR(100)
 );
 
 -- EMPLOYEE TRACKER FOR RECORD CHANGES ON THE TABLE
 CREATE TABLE Employee_Audit (
-    audit_id INT PRIMARY KEY AUTO_INCREMENT,
-    employee_id INT,
+    auditID INT PRIMARY KEY AUTO_INCREMENT,
+    employeeID INT,
     action VARCHAR(10),
-    action_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    action_time TIMESTAMP,
     name VARCHAR(100),
-    workrole VARCHAR(50),
+    workTitle VARCHAR(50),
     Phone VARCHAR(20),
     Email VARCHAR(100),
     Address VARCHAR(100)
@@ -48,20 +49,20 @@ CREATE TABLE Employee_Audit (
 
 -- SUPPLIERS STORAGE TABLE
 CREATE TABLE Suppliers (
-    suppliers_id INT PRIMARY KEY AUTO_INCREMENT,
+    suppliersID INT PRIMARY KEY AUTO_INCREMENT,
     Institution VARCHAR(100),
     products VARCHAR(100),
     DelivererName VARCHAR(100),
-    Phone VARCHAR(20),
+    Phone VARCHAR(20) unique,
     DeliveryTime VARCHAR(50)
 );
 
 -- SUPPLIERS TRACKER FOR RECORD CHANGES ON THE TABLE
 CREATE TABLE Suppliers_Audit (
-    audit_id INT PRIMARY KEY AUTO_INCREMENT,
-    suppliers_id INT,
+    auditID INT PRIMARY KEY AUTO_INCREMENT,
+    suppliersID INT,
     action VARCHAR(10),
-    action_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    action_time TIMESTAMP,
     Institution VARCHAR(100),
     products VARCHAR(100),
     DelivererName VARCHAR(100),
@@ -71,69 +72,69 @@ CREATE TABLE Suppliers_Audit (
 
 -- PRODUCTS STORAGE TABLE
 CREATE TABLE Products (
-    product_id INT PRIMARY KEY AUTO_INCREMENT,
+    productID INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(100) NOT NULL,
     Price DECIMAL(10,2) NOT NULL,
     category VARCHAR(50),
     Expiring  VARCHAR(20),
-    suppliers_id INT,
-    FOREIGN KEY (suppliers_id) REFERENCES Suppliers(suppliers_id)
+    suppliersID INT,
+    FOREIGN KEY (suppliersID) REFERENCES Suppliers(suppliersID)
 );
 
 -- PRODUCTS TRACKER FOR RECORD CHANGES IN THE TABLE
 CREATE TABLE Products_Audit (
-    audit_id INT PRIMARY KEY AUTO_INCREMENT,
-    product_id INT,
+    auditID INT PRIMARY KEY AUTO_INCREMENT,
+    productID INT,
     action VARCHAR(10),
-    action_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    action_time TIMESTAMP,
     name VARCHAR(100),
     Price DECIMAL(10,2),
     category VARCHAR(50),
     Expiring  VARCHAR(20),
-    suppliers_id INT
+    suppliersID INT
 );
 
 -- TRANSACTIONS STORAGE TABLE
 CREATE TABLE Transactions (
-    transaction_id INT PRIMARY KEY AUTO_INCREMENT,
-    customer_id INT,
-    employee_id INT,
+    transactionID INT PRIMARY KEY AUTO_INCREMENT,
+    customerID INT,
+    employeeID INT,
     TransactionDate VARCHAR(20),
     TotalAmount DECIMAL(10,2),
-    FOREIGN KEY (customer_id) REFERENCES Customer(customer_id),
-    FOREIGN KEY (employee_id) REFERENCES Employee(employee_id)
+    FOREIGN KEY (customerID) REFERENCES Customer(customerID) ON DELETE CASCADE,
+    FOREIGN KEY (employeeID) REFERENCES Employee(employeeID)
 );
 
 -- TRANSACTIONS TRACKER FOR RECORD CHANGES ON THE TABLE
 CREATE TABLE Transactions_Audit (
-    audit_id INT PRIMARY KEY AUTO_INCREMENT,
-    transaction_id INT,
+    auditID INT PRIMARY KEY AUTO_INCREMENT,
+    transactionID INT,
     action VARCHAR(10),
-    action_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    customer_id INT,
-    employee_id INT,
+    action_time TIMESTAMP,
+    customerID INT,
+    employeeID INT,
     TransactionDate DATE,
     TotalAmount DECIMAL(10,2)
 );
 
 -- TRANSACTION DETAILS STORAGE TABLE
 CREATE TABLE TransactionDetails (
-    transaction_id INT,
-    product_id INT,
+    transactionID INT,
+    productID INT,
     quantity INT NOT NULL,
     subtotal DECIMAL(10,2),
-    PRIMARY KEY (transaction_id, product_id),
-    FOREIGN KEY (transaction_id) REFERENCES Transactions(transaction_id),
-    FOREIGN KEY (product_id) REFERENCES Products(product_id)
+    PRIMARY KEY (transactionID, productID),
+    FOREIGN KEY (transactionID) REFERENCES Transactions(transactionID),
+    FOREIGN KEY (productID) REFERENCES Products(productID)
 );
 
 -- TRANSACTION DETAILS CHANGES TABLE
 CREATE TABLE TransactionDetails_Audit (
-    audit_id INT PRIMARY KEY AUTO_INCREMENT,
-    transaction_id INT,
-    product_id INT,
+    auditID INT PRIMARY KEY AUTO_INCREMENT,
+    transactionID INT,
+    productID INT,
     action VARCHAR(10),
-    action_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    action_time TIMESTAMP,
     quantity INT,
     subtotal DECIMAL(10,2)
 );
@@ -146,7 +147,7 @@ BEFORE INSERT ON TransactionDetails
 FOR EACH ROW                
 BEGIN
     DECLARE unit_price DECIMAL(10,2);
-    SELECT Price INTO unit_price FROM Products WHERE product_id = NEW.product_id;
+    SELECT Price INTO unit_price FROM Products WHERE productID = NEW.productID;
     SET NEW.subtotal = NEW.quantity * unit_price;
 END
 //
@@ -165,9 +166,9 @@ BEGIN
     SET TotalAmount = (
         SELECT IFNULL(SUM(subtotal), 0)
         FROM TransactionDetails
-        WHERE transaction_id = NEW.transaction_id
+        WHERE transactionID = NEW.transactionID
     )
-    WHERE transaction_id = NEW.transaction_id;
+    WHERE transactionID = NEW.transactionID;
 END
 //
 
@@ -180,13 +181,13 @@ BEGIN
     SET TotalAmount = (
         SELECT IFNULL(SUM(subtotal), 0)
         FROM TransactionDetails
-        WHERE transaction_id = NEW.transaction_id
+        WHERE transactionID = NEW.transactionID
     )
-    WHERE transaction_id = NEW.transaction_id;
+    WHERE transactionID = NEW.transactionID;
 END
 //
 
--- SHOWING THE UPDATE THAT THE TRASACTION HAS BEEN DELETED
+-- SHOWING THE UPDATE THAT THE TRANASCTION HAS BEEN DELETED
 CREATE TRIGGER update_total_after_delete
 AFTER DELETE ON TransactionDetails
 FOR EACH ROW
@@ -195,34 +196,11 @@ BEGIN
     SET TotalAmount = (
         SELECT IFNULL(SUM(subtotal), 0)
         FROM TransactionDetails
-        WHERE transaction_id = OLD.transaction_id
+        WHERE transactionID = OLD.transactionID
     )
-    WHERE transaction_id = OLD.transaction_id;
+    WHERE transactionID = OLD.transactionID;
 END
 //
-
--- After all CREATE TABLE and CREATE TRIGGER STATEMENT
-
-
-SELECT * FROM Customer;
-SELECT * FROM Customer_Audit;
-
-SELECT * FROM Employee;
-SELECT * FROM Employee_Audit;
-
-SELECT * FROM Suppliers;
-SELECT * FROM Suppliers_Audit;
-
-SELECT * FROM Products;
-SELECT * FROM Products_Audit;
-
-SELECT * FROM Transactions;
-SELECT * FROM Transactions_Audit;
-
-SELECT * FROM TransactionDetails;
-SELECT * FROM TransactionDetails_Audit;
-// 
-
 
 -- AUDIT TRIGGERS: MONITORING THE AUTOMATIC ACTIONS
 
@@ -231,8 +209,8 @@ CREATE TRIGGER customer_after_insert
 AFTER INSERT ON Customer
 FOR EACH ROW
 BEGIN
-    INSERT INTO Customer_Audit (customer_id, action, action_time, name, Phone, Email, Address)
-    VALUES (NEW.customer_id, 'INSERT', NOW(), NEW.name, NEW.Phone, NEW.Email, NEW.Address);
+    INSERT INTO Customer_Audit (customerID, action, action_time, name, Phone, Email, Address)
+    VALUES (NEW.customerID, 'INSERT', CONVERT_TZ(NOW(), @@session.time_zone, '+02:00'), NEW.name, NEW.Phone, NEW.Email, NEW.Address);
 END
 //
 
@@ -240,8 +218,8 @@ CREATE TRIGGER customer_after_update
 AFTER UPDATE ON Customer
 FOR EACH ROW
 BEGIN
-    INSERT INTO Customer_Audit (customer_id, action, action_time, name, Phone, Email, Address)
-    VALUES (NEW.customer_id, 'UPDATE', NOW(), NEW.name, NEW.Phone, NEW.Email, NEW.Address);
+    INSERT INTO Customer_Audit (customerID, action, action_time, name, Phone, Email, Address)
+    VALUES (NEW.customerID, 'UPDATE', CONVERT_TZ(NOW(), @@session.time_zone, '+02:00'), NEW.name, NEW.Phone, NEW.Email, NEW.Address);
 END
 //
 
@@ -249,8 +227,8 @@ CREATE TRIGGER customer_after_delete
 AFTER DELETE ON Customer
 FOR EACH ROW
 BEGIN
-    INSERT INTO Customer_Audit (customer_id, action, action_time, name, Phone, Email, Address)
-    VALUES (OLD.customer_id, 'DELETE', NOW(), OLD.name, OLD.Phone, OLD.Email, OLD.Address);
+    INSERT INTO Customer_Audit (customerID, action, action_time, name, Phone, Email, Address)
+    VALUES (OLD.customerID, 'DELETE', CONVERT_TZ(NOW(), @@session.time_zone, '+02:00'), OLD.name, OLD.Phone, OLD.Email, OLD.Address);
 END
 //
 
@@ -259,8 +237,8 @@ CREATE TRIGGER employee_after_insert
 AFTER INSERT ON Employee
 FOR EACH ROW
 BEGIN
-    INSERT INTO Employee_Audit (employee_id, action, action_time, name, workTitle, Phone, Email, Address)
-    VALUES (NEW.employee_id, 'INSERT', NOW(), NEW.name, NEW.workTitle, NEW.Phone, NEW.Email, NEW.Address);
+    INSERT INTO Employee_Audit (employeeID, action, action_time, name, workTitle, Phone, Email, Address)
+    VALUES (NEW.employeeID, 'INSERT', CONVERT_TZ(NOW(), @@session.time_zone, '+02:00'), NEW.name, NEW.workTitle, NEW.Phone, NEW.Email, NEW.Address);
 END
 //
 
@@ -268,8 +246,8 @@ CREATE TRIGGER employee_after_update
 AFTER UPDATE ON Employee
 FOR EACH ROW
 BEGIN
-    INSERT INTO Employee_Audit (employee_id, action, action_time, name, workTitle, Phone, Email, Address)
-    VALUES (NEW.employee_id, 'UPDATE', NOW(), NEW.name, NEW.workTitle, NEW.Phone, NEW.Email, NEW.Address);
+    INSERT INTO Employee_Audit (employeeID, action, action_time, name, workTitle, Phone, Email, Address)
+    VALUES (NEW.employeeID, 'UPDATE', CONVERT_TZ(NOW(), @@session.time_zone, '+02:00'), NEW.name, NEW.workTitle, NEW.Phone, NEW.Email, NEW.Address);
 END
 //
 
@@ -277,8 +255,8 @@ CREATE TRIGGER employee_after_delete
 AFTER DELETE ON Employee
 FOR EACH ROW
 BEGIN
-    INSERT INTO Employee_Audit (employee_id, action, action_time, name, workTitle, Phone, Email, Address)
-    VALUES (OLD.employee_id, 'DELETE', NOW(), OLD.name, OLD.workTitle, OLD.Phone, OLD.Email, OLD.Address);
+    INSERT INTO Employee_Audit (employeeID, action, action_time, name, workTitle, Phone, Email, Address)
+    VALUES (OLD.employeeID, 'DELETE', CONVERT_TZ(NOW(), @@session.time_zone, '+02:00'), OLD.name, OLD.workTitle, OLD.Phone, OLD.Email, OLD.Address);
 END
 //
 
@@ -287,8 +265,8 @@ CREATE TRIGGER suppliers_after_insert
 AFTER INSERT ON Suppliers
 FOR EACH ROW
 BEGIN
-    INSERT INTO Suppliers_Audit (suppliers_id, action, action_time, Institution, products, DelivererName, Phone, DeliveryTime)
-    VALUES (NEW.suppliers_id, 'INSERT', NOW(), NEW.Institution, NEW.products, NEW.DelivererName, NEW.Phone, NEW.DeliveryTime);
+    INSERT INTO Suppliers_Audit (suppliersID, action, action_time, Institution, products, DelivererName, Phone, DeliveryTime)
+    VALUES (NEW.suppliersID, 'INSERT', CONVERT_TZ(NOW(), @@session.time_zone, '+02:00'), NEW.Institution, NEW.products, NEW.DelivererName, NEW.Phone, NEW.DeliveryTime);
 END
 //
 
@@ -296,8 +274,8 @@ CREATE TRIGGER suppliers_after_update
 AFTER UPDATE ON Suppliers
 FOR EACH ROW
 BEGIN
-    INSERT INTO Suppliers_Audit (suppliers_id, action, action_time, Institution, products, DelivererName, Phone, DeliveryTime)
-    VALUES (NEW.suppliers_id, 'UPDATE', NOW(), NEW.Institution, NEW.products, NEW.DelivererName, NEW.Phone, NEW.DeliveryTime);
+    INSERT INTO Suppliers_Audit (suppliersID, action, action_time, Institution, products, DelivererName, Phone, DeliveryTime)
+    VALUES (NEW.suppliersID, 'UPDATE', CONVERT_TZ(NOW(), @@session.time_zone, '+02:00'), NEW.Institution, NEW.products, NEW.DelivererName, NEW.Phone, NEW.DeliveryTime);
 END
 //
 
@@ -305,8 +283,8 @@ CREATE TRIGGER suppliers_after_delete
 AFTER DELETE ON Suppliers
 FOR EACH ROW
 BEGIN
-    INSERT INTO Suppliers_Audit (suppliers_id, action, action_time, Institution, products, DelivererName, Phone, DeliveryTime)
-    VALUES (OLD.suppliers_id, 'DELETE', NOW(), OLD.Institution, OLD.products, OLD.DelivererName, OLD.Phone, OLD.DeliveryTime);
+    INSERT INTO Suppliers_Audit (suppliersID, action, action_time, Institution, products, DelivererName, Phone, DeliveryTime)
+    VALUES (OLD.suppliersID, 'DELETE', CONVERT_TZ(NOW(), @@session.time_zone, '+02:00'), OLD.Institution, OLD.products, OLD.DelivererName, OLD.Phone, OLD.DeliveryTime);
 END
 //
 
@@ -315,35 +293,30 @@ CREATE TRIGGER products_after_insert
 AFTER INSERT ON Products
 FOR EACH ROW
 BEGIN
-    INSERT INTO Products_Audit (product_id, action, action_time, name, Price, category, Expiring, suppliers_id)
-    VALUES (NEW.product_id, 'INSERT', NOW(), NEW.name, NEW.Price, NEW.category, NEW.Expiring, NEW.suppliers_id);
+    INSERT INTO Products_Audit (productID, action, action_time, name, Price, category, Expiring, suppliersID)
+    VALUES (NEW.productID, 'INSERT', CONVERT_TZ(NOW(), @@session.time_zone, '+02:00'), NEW.name, NEW.Price, NEW.category, NEW.Expiring, NEW.suppliersID);
 END
 //
 
--- SHOWING THE PRODUCTS AFTER UPDATE
 CREATE TRIGGER products_after_update  
 AFTER UPDATE ON Products
 FOR EACH ROW
 BEGIN
-    INSERT INTO Products_Audit (product_id, action, action_time, name, Price, category, Expiring, suppliers_id)
-    VALUES (NEW.product_id, 'UPDATE', NOW(), NEW.name, NEW.Price, NEW.category, NEW.Expiring, NEW.suppliers_id);
+    INSERT INTO Products_Audit (productID, action, action_time, name, Price, category, Expiring, suppliersID)
+    VALUES (NEW.productID, 'UPDATE', CONVERT_TZ(NOW(), @@session.time_zone, '+02:00'), NEW.name, NEW.Price, NEW.category, NEW.Expiring, NEW.suppliersID);
 END
 //
 
--- SHOW THAT TRULY THE PRODUCTS HAS BEEN DELETED
 CREATE TRIGGER products_after_delete    
 AFTER DELETE ON Products
 FOR EACH ROW
 BEGIN
-    INSERT INTO Products_Audit (product_id, action, action_time, name, Price, category, Expiring, suppliers_id)
-    VALUES (OLD.product_id, 'DELETE', NOW(), OLD.name, OLD.Price, OLD.category, OLD.Expiring, OLD.suppliers_id);
+    INSERT INTO Products_Audit (productID, action, action_time, name, Price, category, Expiring, suppliersID)
+    VALUES (OLD.productID, 'DELETE', CONVERT_TZ(NOW(), @@session.time_zone, '+02:00'), OLD.name, OLD.Price, OLD.category, OLD.Expiring, OLD.suppliersID);
 END
 //
 
-
-
 DELIMITER ;
-
 
 
 
